8d850b9a7d66fd6cde5866919b59fd7aecaef7a5
jes0321 <157565937+jes0321@users.noreply.github.com>
2024-09-13 12:26:38 +0900

    feat : unit sort sys

--- Assets/01_Scripts/JES/Unit/UnitUpgrade.cs
+++ Assets/01_Scripts/JES/Unit/UnitUpgrade.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitUpgrade : MonoBehaviour
 {
-    public UnitUpgradeData unitData;
+    private UnitUpgradeData _unitData;//유닛 데이터 저장
+    private Image _imageCompo;//이미지 컴포
 
+    [SerializeField]
+    private GameObject _panel;//껐다 킬 패널
+    [SerializeField] private TextMeshProUGUI _nameText; // 이름 텍스트
 
-
+    
     public void Initalize(UpgradeManager manager, UnitUpgradeData data)
     {
+        _imageCompo = GetComponent<Image>();
+        
+        _unitData = data;
+        _imageCompo.sprite = _unitData.sprite;
+        
         manager.OnChangeEvent += HandleChangeEvent;
-        unitData = data;
+        _panel.SetActive(manager.index!=_unitData.index);
+        
+        _nameText.text = _unitData.name;
     }
 
     private void HandleChangeEvent(int index, bool _isRight)
     {
-        //내꺼랑 인덱스 같으면 키고 아니면 끄는 작업
+        _panel.SetActive(index!=_unitData.index);
+        
+        int xPos = _isRight ? -500 : 500;
+        
+        transform.DOMoveX( transform.position.x+xPos, 0.75f).SetEase(Ease.OutQuint);
     }
 }
--- Assets/01_Scripts/JES/Unit/UpgradeManager.cs
+++ Assets/01_Scripts/JES/Unit/UpgradeManager.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UpgradeManager : MonoSingleton<UpgradeManager>
 {
+    [SerializeField]
     private List<UnitUpgradeData> _dataList;
     [SerializeField] private UnitUpgrade _unitPrefab;
 
+    [SerializeField] private float _xPadding;
+    private float curPadding;
+
+
+    [SerializeField] private GameObject _lBtn, _rBtn;//좌우 버튼
+    
+    
     public event Action<int, bool> OnChangeEvent;
     public int index = 0;
     
@@ -14,12 +23,39 @@ public class UpgradeManager : MonoSingleton<UpgradeManager>
     {
         foreach (UnitUpgradeData upgradeData in _dataList)
         {
-            UnitUpgrade unit = Instantiate(_unitPrefab, transform);
-            
-            //패딩 해야함
+            UnitUpgrade unit = Instantiate(_unitPrefab, transform); //소환
+
+            unit.transform.localPosition = new Vector3(_xPadding * curPadding, 0, 0); //패딩
+
             unit.Initalize(this, upgradeData);
-            
-            
+
+            curPadding++;
+
         }
+
+        _lBtn.SetActive(false);
+    }
+
+    public void LeftBtnClick()
+    {
+        if (index > 0)
+        {
+            OnChangeEvent?.Invoke(--index, false);
+            _rBtn.SetActive(true);
+        }
+        
+        if(index==0)
+            _lBtn.SetActive(false);
+    }
+    public void RightBtnClick()
+    {
+        if (index < 5)
+        {
+            OnChangeEvent?.Invoke(++index, true);
+            _lBtn.SetActive(true);
+        }
+        
+        if(index==5)
+            _rBtn.SetActive(false);
     }
 }
