182f88b7b2679da9f318579d54825b69607da9f8
jes0321 <157565937+jes0321@users.noreply.github.com>
2024-09-22 00:45:07 +0900

    feat :
    ?

---
+++ Assets/01_Scripts/JES/Unit/Combat/UnitDataSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "SO/Unit/Combat/Data")]
+public class UnitDataSO : ScriptableObject
+{
+
+}
